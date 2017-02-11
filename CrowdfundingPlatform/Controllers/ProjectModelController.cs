b36cff0c3af406f7812f34230935bee6348bdc49
delta750 <nkapetanas@hotmail.com>
2017-02-11 02:10:43 +0200

    new project

---
+++ CrowdfundingPlatform/Controllers/ProjectModelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrowdfundingPlatform.Controllers
+{
+    public class ProjectModelController : Controller
+    {
+        // GET: ProjectModel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
